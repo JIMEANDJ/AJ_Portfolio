bd3ba9c360bc2eaf1b3aa680a55689288e5347e2
Andres Jimenez <129030470+JIMEANDJ@users.noreply.github.com>
2024-08-05 20:32:31 -0500

    Update Hireme.jsx

--- src/components/Hireme.jsx
+++ src/components/Hireme.jsx
@@ -12,9 +12,7 @@ const Hireme = () => {
         <h2 className="title" data-aos="fade-down">
           {t("hireme.title")}
         </h2>
-        <h4 className="subtitle" data-aos="fade-down">
-          {t("hireme.subtitle")}
-        </h4>
+
         <br />
         <div className="flex items-center md:flex-row flex-col-reverse ">
           <img
